33680831e8d01512e16fc8c3c2600b0d01e42a72
Luiz Daniel Lima <luizdml@gmail.com>
2023-02-16 17:10:31 -0500

    card style with country name region flag and population

--- src/components/CountryCard/CountryCard.styled.ts
+++ src/components/CountryCard/CountryCard.styled.ts
@@ -1,30 +1,61 @@
 import styled from 'styled-components';
 
-export const CountryCardWrapper = styled.div`
-  padding: 20px;
-  border: 1px solid #eee;
-  width: 160px;
-  background-color: #fff;
-`;
-
 export const CountryCardHeader = styled.div`
+  position: relative;
   min-height: 2rem;
+  width: 100%;
+  text-align: center;
 `;
 
 export const CountryCardTitle = styled.p<{ fontSize: string }>`
   font-size: ${({ fontSize }) => fontSize};
-  font-weight: 600;
-  text-align: center;
+  font-weight: 800;
+`;
+
+export const CountryCardRegion = styled.p`
+  font-size: 0.7rem;
+  font-weight: 400;
+`;
+
+export const CountryCardWrapper = styled.div`
+  padding: 16px;
+  cursor: pointer;
+  border: 1px solid #eee;
+  width: 160px;
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  &:hover {
+    box-shadow: 0px 10px 15px -3px rgba(16, 24, 40, 0.1),
+      0px 4px 6px -4px rgba(16, 24, 40, 0.1);
+
+    img {
+      filter: grayscale(0%);
+    }
+
+    ${CountryCardTitle} {
+      color: black;
+      font-weight: 900;
+    }
+  }
 `;
 
 export const CountryCardFlagWrapper = styled.figure`
   height: 90px;
   width: 100%;
+  margin: 16px 0;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
 
   img {
+    filter: grayscale(80%);
     width: auto;
     height: auto;
     max-width: 100%;
@@ -34,4 +65,13 @@ export const CountryCardFlagWrapper = styled.figure`
 
 export const CountryCardFlag = styled.img`
   border: 1px solid #ccc;
+`;
+
+export const CountryCardBody = styled.div`
+  text-align: center;
+  width: 100%;
+`;
+export const CountryCardFooter = styled.div`
+  text-align: center;
+  width: 100%;
 `;
--- src/components/CountryCard/CountryCard.tsx
+++ src/components/CountryCard/CountryCard.tsx
@@ -1,10 +1,14 @@
 import React, { FC, useMemo } from 'react';
 
 import { CountryType } from '../../models/Country';
+import { formatPopulation } from '../../utils/formatPopulation';
 import {
+  CountryCardBody,
   CountryCardFlag,
   CountryCardFlagWrapper,
+  CountryCardFooter,
   CountryCardHeader,
+  CountryCardRegion,
   CountryCardTitle,
   CountryCardWrapper,
 } from './CountryCard.styled';
@@ -14,23 +18,33 @@ const CountryCard: FC<{ loading: boolean; country: CountryType }> = ({
   country,
 }) => {
   const getFontSize = useMemo(() => {
-    if (country.name.common.length > 16) {
+    if (country.name.common.length > 24) {
       return '0.65rem';
     }
-    if (country.name.common.length > 12) {
+    if (country.name.common.length > 20) {
       return '0.75rem';
     }
-    return '0.9rem';
+    if (country.name.common.length > 16) {
+      return '0.85rem';
+    }
+    return '1rem';
   }, [country.name.common.length]);
   return (
-    <CountryCardWrapper>
+    <CountryCardWrapper
+      key={`${country.name.common}_${country.capital}`}
+    >
       {!country && loading && <p>Loading...</p>}
       {country && !loading && (
-        <div key={`${country.name.common}_${country.capital}`}>
+        <>
           <CountryCardHeader>
             <CountryCardTitle fontSize={getFontSize}>
               {country.name.common.toUpperCase()}
             </CountryCardTitle>
+            {country.region[0] && (
+              <CountryCardRegion>
+                {country.region.toUpperCase()}
+              </CountryCardRegion>
+            )}
           </CountryCardHeader>
           <CountryCardFlagWrapper>
             <CountryCardFlag
@@ -40,7 +54,11 @@ const CountryCard: FC<{ loading: boolean; country: CountryType }> = ({
               alt={`${country.name.common} flag`}
             />
           </CountryCardFlagWrapper>
-        </div>
+          {/* <CountryCardBody></CountryCardBody> */}
+          <CountryCardFooter>
+            <p>{formatPopulation(country.population)}</p>
+          </CountryCardFooter>
+        </>
       )}
     </CountryCardWrapper>
   );
